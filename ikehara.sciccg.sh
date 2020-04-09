dcb699950c2472cb6257091d97e924b176109e45
KotaIkehara <ikehara.aurora95@gmail.com>
2020-04-09 14:50:44 +0900

    Try threshold=3,4,5.

--- ikehara.sciccg.sh
+++ ikehara.sciccg.sh
@@ -8,12 +8,6 @@
 #PJM -j
 #------- Program execution -------#
 export OMP_NUM_THREADS=1
-./sciccg.out mtx/thermal2.mtx 1
-./sciccg.out mtx/thermal2.mtx 2
-./sciccg.out mtx/thermal2.mtx 3
-./sciccg.out mtx/thermal2.mtx 4
-./sciccg.out mtx/thermal2.mtx 5
-./sciccg.out mtx/thermal2.mtx 6
-./sciccg.out mtx/thermal2.mtx 7
-./sciccg.out mtx/thermal2.mtx 8
-./sciccg.out mtx/thermal2.mtx 9
+./sciccg.out mtx/G3_circuit.mtx 3
+./sciccg.out mtx/G3_circuit.mtx 4
+./sciccg.out mtx/G3_circuit.mtx 5
