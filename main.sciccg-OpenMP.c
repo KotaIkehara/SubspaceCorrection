8c510298aed7c9f7db0b1a3bb32b443767d9def3
KotaIkehara <ikehara.aurora95@gmail.com>
2020-08-31 14:29:52 +0900

    Expand parallel region

--- main.sciccg-OpenMP.c
+++ main.sciccg-OpenMP.c
@@ -221,246 +221,267 @@ int main(int argc, char *argv[]) {
       fprintf(fp, "#ite residual of %s\n", argv[1]);
     }
 
-    if (zite == 0) {
-      ganma = 1.0;
-#pragma omp parallel for
-      for (i = 0; i < n; i++) {
-        diag[i] = 0.0;
-        iuhead[i] = 0;
-      }
-#pragma omp parallel for
-      for (i = 0; i < nnonzero; i++) {
-        iucol[i] = 0;
-        u[i] = 0.0;
-      }
-
-      ku = 0;
-      iuhead[0] = 1;
-      // TODO:OMP single
-      for (i = 0; i < n; i++) {
-        ku = 0;
-        kl = 0;
-        for (j = row_ptr[i]; j < row_ptr[i + 1]; j++) {
-          jj = col_ind[j];
-          if (jj == i) {
-            diag[i] = val[j] * ganma;
-          } else if (jj < i) {
-            kl++;
-          } else if (jj > i) {
-            iucol[ku + iuhead[i]] = jj;
-            u[ku + iuhead[i]] = val[j];
-            ku++;
-          } else {
-            printf("error");
-            break;
-          }
-          iuhead[i + 1] = iuhead[i] + ku;
+#pragma omp parallel
+    {
+      if (zite == 0) {
+        ganma = 1.0;
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          diag[i] = 0.0;
+          iuhead[i] = 0;
         }
-      }
+#pragma omp for
+        for (i = 0; i < nnonzero; i++) {
+          iucol[i] = 0;
+          u[i] = 0.0;
+        }
 
-      // IC decomposition TODO
-      for (i = 0; i < n; i++) {
-        for (j = iuhead[i]; j < iuhead[i + 1]; j++) {
-          jj = iucol[j];
-
-          diag[iucol[j]] = diag[iucol[j]] - u[j] * u[j] / diag[i];
-
-          for (jp = j + 1; jp < iuhead[i + 1]; jp++) {
-            jjp = iucol[jp];
-            for (ji = iuhead[jj]; ji < iuhead[jj + 1]; ji++) {
-              if (iucol[ji] == jjp) {
-                u[ji] = u[ji] - u[j] * u[jp] / diag[i];
-                // exit??
+#pragma omp single
+        {
+          ku = 0;
+          iuhead[0] = 1;
+          // TODO:OMP single
+          for (i = 0; i < n; i++) {
+            ku = 0;
+            kl = 0;
+            for (j = row_ptr[i]; j < row_ptr[i + 1]; j++) {
+              jj = col_ind[j];
+              if (jj == i) {
+                diag[i] = val[j] * ganma;
+              } else if (jj < i) {
+                kl++;
+              } else if (jj > i) {
+                iucol[ku + iuhead[i]] = jj;
+                u[ku + iuhead[i]] = val[j];
+                ku++;
+              } else {
+                printf("error");
                 break;
               }
+              iuhead[i + 1] = iuhead[i] + ku;
+            }
+          }
+
+          // IC decomposition TODO
+          for (i = 0; i < n; i++) {
+            for (j = iuhead[i]; j < iuhead[i + 1]; j++) {
+              jj = iucol[j];
+
+              diag[iucol[j]] = diag[iucol[j]] - u[j] * u[j] / diag[i];
+
+              for (jp = j + 1; jp < iuhead[i + 1]; jp++) {
+                jjp = iucol[jp];
+                for (ji = iuhead[jj]; ji < iuhead[jj + 1]; ji++) {
+                  if (iucol[ji] == jjp) {
+                    u[ji] = u[ji] - u[j] * u[jp] / diag[i];
+                    // exit??
+                    break;
+                  }
+                }
+              }
+            }
+            if (fabs(diag[i]) < 0.001) {
+              break;
             }
           }
         }
-        if (fabs(diag[i]) < 0.001) {
-          break;
+        // end IC decomposition
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          diag[i] = 1.0 / diag[i];
         }
       }
-      // end IC decomposition
-
-#pragma omp parallel for
-      for (i = 0; i < n; i++) {
-        diag[i] = 1.0 / diag[i];
+#pragma omp single
+      {
+        bnorm = 0.0;
+        for (i = 0; i < n; i++) {
+          b[i] = rand() / (double)RAND_MAX;
+          // if (zite == 0) b[i]=1.0;
+          // b[i] = 1.0;
+          bnorm += fabs(b[i]) * fabs(b[i]);
+        }
       }
-    }
-
-    bnorm = 0.0;
-    for (i = 0; i < n; i++) {
-      b[i] = rand() / (double)RAND_MAX;
-      // if (zite == 0) b[i]=1.0;
-      // b[i] = 1.0;
-      bnorm += fabs(b[i]) * fabs(b[i]);
-    }
 
 //     printf("bnorm = %f , %d\n", bnorm, n);
-#pragma omp parallel for
-    for (i = 0; i < n; i++) {
-      solx[i] = 0.0;
-      //      diag[i] = 1.0 / diag[i];
-    }
-
+#pragma omp for
+      for (i = 0; i < n; i++) {
+        solx[i] = 0.0;
+        //      diag[i] = 1.0 / diag[i];
+      }
 // calc Residual
-// TODO: OMP
-#pragma omp parallel for private(ar0, j, jj)
-    for (i = 0; i < n; i++) {
-      ar0 = 0.0;
-      for (j = row_ptr[i]; j < row_ptr[i + 1]; j++) {
-        jj = col_ind[j];
-        ar0 += val[j] * solx[jj];
-      }
-      r[i] = b[i] - ar0;
-    }
-    // end calc residual
-
-    if (zite == 0) {
-      pivot = (lapack_int *)calloc(sizeof(lapack_int), m_max);
-    }
-    cgrop = 0.0;
-
-    if (zite == 1) {
-#pragma omp parallel for private(j)
-      for (i = 0; i < m_max; i++) {
-        for (j = 0; j < n; j++) {
-          ab[i * n + j] = 0.0;
-        }
-      }
-
-#pragma omp parallel for private(j, k)
-      for (i = 0; i < m_max; i++) {
-        for (j = 0; j < n; j++) {
-          for (k = row_ptr[j]; k < row_ptr[j + 1]; k++) {
-            ab[i * n + j] += val[k] * B[i * n + col_ind[k]];
-          }
-        }
-      }
-
-      cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m_max, m_max, n, 1.0,
-                  B, n, ab, n, 0.0, bab, m_max);
-
-      // LU decomposition
-      LAPACKE_dgetrf(LAPACK_COL_MAJOR, m_max, m_max, bab, m_max, pivot);
-    }
-
-    for (ite = 1; ite < nitecg; ite++) {
-#pragma omp parallel for
+#pragma omp for private(ar0, j, jj)
       for (i = 0; i < n; i++) {
-        z[i] = r[i];
-      }
-      // Forward substitution
-      // TODO: OMP
-      for (i = 0; i < n; i++) {
-        for (j = iuhead[i]; j < iuhead[i + 1]; j++) {
-          jj = iucol[j];
-          z[jj] += -z[i] * u[j] * diag[i];
-        }
-      }
-      // end Forward substitution
-
-      z[n - 1] = z[n - 1] * diag[n - 1];
-
-      // backward substitution
-      // TODO: OMP
-      for (i = n - 2; i >= 0; --i) {
-        for (j = iuhead[i + 1] - 1; j >= iuhead[i]; --j) {
-          jj = iucol[j];
-          z[i] += -u[j] * z[jj];
-        }
-        z[i] *= diag[i];
-      }
-      // end backward substitutions
-
-      // ignore IC
-      // for (i = 0; i < n; i++)
-      // {
-      //   z[i] = r[i];
-      // }
-
-      // begin SC
-      if (zite > 0) {
-        // Step1. Compute f = B^T * r
-        cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m_max, 1, n, 1.0,
-                    B, n, r, n, 0.0, f, m_max);
-        // Step2. Solve (B^TAB)u = f
-        // forward/backward substitution
-        LAPACKE_dgetrs(LAPACK_COL_MAJOR, 'N', m_max, 1, bab, m_max, pivot, f,
-                       m_max);
-
-        // Step3. Compute Zc = Z + Bu
-        /***Compute Bu ***/
-        cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, 1, m_max, 1.0,
-                    B, n, f, m_max, 0.0, Bu, n);
-#pragma omp parallel for
-        for (i = 0; i < n; i++) {
-          z[i] += Bu[i];
-        }
-      }
-      // end SC
-
-      // TODO single or barrier
-      cgropp = cgrop;
-      cgrop = 0.0;
-#pragma omp parallel for reduction(+ : cgrop)
-      for (i = 0; i < n; i++) {
-        cgrop += r[i] * z[i];
-      }
-
-      if (ite == 1) {
-#pragma omp parallel for
-        for (i = 0; i < n; i++) {
-          pn[i] = z[i];
-        }
-      } else {
-        beta = cgrop / cgropp;
-#pragma omp parallel for
-        for (i = 0; i < n; i++) {
-          pn[i] = z[i] + beta * p[i];
-        }
-      }
-#pragma omp parallel for
-      for (i = 0; i < n; i++) {
-        q[i] = 0.0;
-      }
-
-#pragma omp parallel for private(j, jj)
-      for (i = 0; i < n; i++) {
+        ar0 = 0.0;
         for (j = row_ptr[i]; j < row_ptr[i + 1]; j++) {
           jj = col_ind[j];
-          q[i] += val[j] * pn[jj];
+          ar0 += val[j] * solx[jj];
+        }
+        r[i] = b[i] - ar0;
+      }
+      // end calc residual
+
+#pragma omp single
+      {
+        if (zite == 0) {
+          pivot = (lapack_int *)calloc(sizeof(lapack_int), m_max);
+        }
+        cgrop = 0.0;
+      }
+
+      if (zite == 1) {
+#pragma omp for private(j)
+        for (i = 0; i < m_max; i++) {
+          for (j = 0; j < n; j++) {
+            ab[i * n + j] = 0.0;
+          }
+        }
+
+#pragma omp for private(j, k)
+        for (i = 0; i < m_max; i++) {
+          for (j = 0; j < n; j++) {
+            for (k = row_ptr[j]; k < row_ptr[j + 1]; k++) {
+              ab[i * n + j] += val[k] * B[i * n + col_ind[k]];
+            }
+          }
+        }
+#pragma omp single
+        {
+          cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m_max, m_max, n,
+                      1.0, B, n, ab, n, 0.0, bab, m_max);
+          // LU decomposition
+          LAPACKE_dgetrf(LAPACK_COL_MAJOR, m_max, m_max, bab, m_max, pivot);
         }
       }
-      // TODO single
-      alphat = 0.0;
-#pragma omp parallel for reduction(+ : alphat)
-      for (i = 0; i < n; i++) {
-        alphat += pn[i] * q[i];
-      }
-      alpha = cgrop / alphat;
+    }  // end of the parallel region
 
-#pragma omp parallel for
-      for (i = 0; i < n; i++) {
-        solx[i] += alpha * pn[i];
-        r[i] -= alpha * q[i];
-      }
-#pragma omp parallel for
-      for (i = 0; i < n; i++) {
-        p[i] = pn[i];
-      }
+    for (ite = 1; ite < nitecg; ite++) {
+#pragma omp parallel
+      {
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          z[i] = r[i];
+        }
+// TODO: OMP
+#pragma omp single
+        {
+          // Forward substitution
+          for (i = 0; i < n; i++) {
+            for (j = iuhead[i]; j < iuhead[i + 1]; j++) {
+              jj = iucol[j];
+              z[jj] += -z[i] * u[j] * diag[i];
+            }
+          }  // end Forward substitution
 
-      rnorm = 0.0;
+          z[n - 1] = z[n - 1] * diag[n - 1];
 
-#pragma omp parallel for reduction(+ : rnorm)
-      for (i = 0; i < n; i++) {
-        rnorm += fabs(r[i]) * fabs(r[i]);
-      }
+          // backward substitution
+          // TODO: OMP
+          for (i = n - 2; i >= 0; --i) {
+            for (j = iuhead[i + 1] - 1; j >= iuhead[i]; --j) {
+              jj = iucol[j];
+              z[i] += -u[j] * z[jj];
+            }
+            z[i] *= diag[i];
+          }
+        }  // end backward substitutions
 
-      //  printf("ite:%d, %lf\n", ite, sqrt(rnorm / bnorm)); //収束判定
-      if (zite == 1) {
-        fprintf(fp, "%d %lf\n", ite, sqrt(rnorm / bnorm));
-      }
+        // ignore IC
+        // for (i = 0; i < n; i++)
+        // {
+        //   z[i] = r[i];
+        // }
+
+        // begin SC
+        if (zite > 0) {
+#pragma omp single
+          {
+            // Step1. Compute f = B^T * r
+            cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m_max, 1, n,
+                        1.0, B, n, r, n, 0.0, f, m_max);
+            // Step2. Solve (B^TAB)u = f
+            // forward/backward substitution
+            LAPACKE_dgetrs(LAPACK_COL_MAJOR, 'N', m_max, 1, bab, m_max, pivot,
+                           f, m_max);
+
+            // Step3. Compute Zc = Z + Bu
+            /***Compute Bu ***/
+            cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, 1, m_max,
+                        1.0, B, n, f, m_max, 0.0, Bu, n);
+          }
+#pragma omp for
+          for (i = 0; i < n; i++) {
+            z[i] += Bu[i];
+          }
+        }
+        // end SC
+
+#pragma omp single
+        {
+          cgropp = cgrop;
+          cgrop = 0.0;
+        }
+#pragma omp for reduction(+ : cgrop)
+        for (i = 0; i < n; i++) {
+          cgrop += r[i] * z[i];
+        }
+
+        if (ite == 1) {
+#pragma omp for
+          for (i = 0; i < n; i++) {
+            pn[i] = z[i];
+          }
+        } else {
+          beta = cgrop / cgropp;
+#pragma omp for
+          for (i = 0; i < n; i++) {
+            pn[i] = z[i] + beta * p[i];
+          }
+        }
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          q[i] = 0.0;
+        }
+
+#pragma omp for private(j, jj)
+        for (i = 0; i < n; i++) {
+          for (j = row_ptr[i]; j < row_ptr[i + 1]; j++) {
+            jj = col_ind[j];
+            q[i] += val[j] * pn[jj];
+          }
+        }
+// TODO single
+#pragma omp single
+        { alphat = 0.0; }
+#pragma omp for reduction(+ : alphat)
+        for (i = 0; i < n; i++) {
+          alphat += pn[i] * q[i];
+        }
+#pragma omp single
+        { alpha = cgrop / alphat; }
+
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          solx[i] += alpha * pn[i];
+          r[i] -= alpha * q[i];
+        }
+#pragma omp for
+        for (i = 0; i < n; i++) {
+          p[i] = pn[i];
+        }
+
+#pragma omp single
+        { rnorm = 0.0; }
+
+#pragma omp for reduction(+ : rnorm)
+        for (i = 0; i < n; i++) {
+          rnorm += fabs(r[i]) * fabs(r[i]);
+        }
+
+        //  printf("ite:%d, %lf\n", ite, sqrt(rnorm / bnorm)); //収束判定
+        if (zite == 1) {
+          fprintf(fp, "%d %lf\n", ite, sqrt(rnorm / bnorm));
+        }
+      }  // end of parallel region
 
       if (sqrt(rnorm / bnorm) < err) {
         //       t1 = get_time();
@@ -491,37 +512,38 @@ int main(int argc, char *argv[]) {
     }  // end ICCG
 
     if (zite == 0) {
-// e = x - x~
-#pragma omp parallel for private(j)
-      for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-          _solx[(i * n) + j] = solx[j] - _solx[(i * n) + j];
-        }
-      }
-
-      /*--- Modified Gram-Schmidt orthogonalization ---*/
       double *enorm, *er, *eq;
       enorm = (double *)malloc(sizeof(double) * m);
       er = (double *)malloc(sizeof(double) * (m * m));
       eq = (double *)malloc(sizeof(double) * (m * n));
-
-      for (i = 0; i < m; i++) {
-        enorm[i] = 0;
-      }
-      for (i = 0; i < m; i++) {
-        for (j = 0; j < m; j++) {
-          er[i * m + j] = 0;
+#pragma omp parallel
+      {
+// e = x - x~
+#pragma omp for private(j)
+        for (i = 0; i < m; i++) {
+          for (j = 0; j < n; j++) {
+            _solx[(i * n) + j] = solx[j] - _solx[(i * n) + j];
+          }
         }
-      }
 
-#pragma omp parallel for private(j)
-      for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-          enorm[i] += _solx[i * n + j] * _solx[i * n + j];
+        /*--- Modified Gram-Schmidt orthogonalization ---*/
+        for (i = 0; i < m; i++) {
+          enorm[i] = 0;
         }
-        enorm[i] = sqrt(enorm[i]);
-      }
+        for (i = 0; i < m; i++) {
+          for (j = 0; j < m; j++) {
+            er[i * m + j] = 0;
+          }
+        }
+#pragma omp for private(j)
+        for (i = 0; i < m; i++) {
+          for (j = 0; j < n; j++) {
+            enorm[i] += _solx[i * n + j] * _solx[i * n + j];
+          }
+          enorm[i] = sqrt(enorm[i]);
+        }
 
+      }  // end of parallel region
       // TODO: OMP
       for (i = 0; i < m; i++) {
         er[i * m + i] = enorm[i];
@@ -547,23 +569,25 @@ int main(int argc, char *argv[]) {
       X2 = (double *)malloc(sizeof(double) * (m * m));
       Y = (double *)malloc(m * sizeof(double));
 
-      for (i = 0; i < m; i++) {
-#pragma omp parallel for
-        for (j = 0; j < n; j++) {
-          ae[i * n + j] = 0;
-        }
-      }
-
-#pragma omp parallel for private(k, j, jj)
-      for (i = 0; i < m; i++) {
-        for (k = 0; k < n; k++) {
-          for (j = row_ptr[k]; j < row_ptr[k + 1]; j++) {
-            jj = col_ind[j];
-            ae[i * n + k] += val[j] * eq[i * n + jj];
+#pragma omp parallel
+      {
+#pragma omp for private(j)
+        for (i = 0; i < m; i++) {
+          for (j = 0; j < n; j++) {
+            ae[i * n + j] = 0;
           }
         }
-      }
 
+#pragma omp for private(k, j, jj)
+        for (i = 0; i < m; i++) {
+          for (k = 0; k < n; k++) {
+            for (j = row_ptr[k]; j < row_ptr[k + 1]; j++) {
+              jj = col_ind[j];
+              ae[i * n + k] += val[j] * eq[i * n + jj];
+            }
+          }
+        }
+      }  // end of the parallel region
       // X = eq^T * ae
       cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m, m, n, 1.0, eq, n,
                   ae, n, 0.0, X, m);
