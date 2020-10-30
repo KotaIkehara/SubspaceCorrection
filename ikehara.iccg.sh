a38181668e8aadc408e4090d6a5e81354a2dc408
KotaIkehara <ikehara.aurora95@gmail.com>
2020-10-30 18:05:53 +0900

    Fix .sh file

--- ikehara.iccg.sh
+++ ikehara.iccg.sh
@@ -1,13 +1,21 @@
-#!/bin/sh 
+#!/bin/sh
 #------ pjsub option --------#
 #PJM -L rscgrp=i21519a
 #PJM -L node=1
 #PJM --mpi proc=1
-#PJM -L elapse=1:00:00 
+#PJM -L elapse=24:00:00 
 #PJM -g i21519
 #PJM -j
 #------- Program execution -------#
-export OMP_NUM_THREADS=1
-./iccg.out mtx/G3_circuit.mtx 3
-./iccg.out mtx/G3_circuit.mtx 4
-./iccg.out mtx/G3_circuit.mtx 5
+
+# mtx=("Queen_4147")
+# mtx=(Bump_2911)
+mtx=("G3_circuit" "Flan_1565" "Hook_1498")
+
+thread=40
+
+for var in ${mtx[@]}
+do 
+  export MKL_DYNAMIC=FALSE MKL_NUM_THREADS=${thread} OMP_NUM_THREADS=${thread} OMP_NESTED=TRUE KMP_AFFINITY=noverbose,granularity=fine,scatter
+  ./iccg-OpenMP.out mtx/${var}.mtx 3
+done
